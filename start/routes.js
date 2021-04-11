feaac639ef32cb3938878c9dc881cda948662c39
Anderson Santos <anderson.santos@tuta.io>
2021-04-11 18:21:41 -0300

    feat: add routes

--- start/routes.js
+++ start/routes.js
@@ -1,21 +1,8 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('/users', 'UserController.create')
+Route.post('/auth', 'AuthenticationController.auth')
+Route.post('/reset-password', 'ResetPasswordController.reset')
+Route.put('/reset-password', 'ResetPasswordController.update')
