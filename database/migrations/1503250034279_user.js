cd1d989abbbcf207249c3081b5465a6e71093222
Anderson Santos <anderson.santos@tuta.io>
2021-04-11 18:13:36 -0300

    chore: update user migration

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -10,6 +10,8 @@ class UserSchema extends Schema {
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.string('reset_password_token')
+      table.timestamp('reset_password_created_at')
       table.timestamps()
     })
   }
