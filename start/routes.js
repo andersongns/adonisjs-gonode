fb319c77d9578e6c69cb89509cc7d478d4405e24
Anderson Santos <anderson.santos@tuta.io>
2021-04-11 19:26:35 -0300

    feat: create file routes

--- start/routes.js
+++ start/routes.js
@@ -6,3 +6,6 @@ Route.post('/users', 'UserController.create')
 Route.post('/auth', 'AuthenticationController.auth')
 Route.post('/reset-password', 'ResetPasswordController.reset')
 Route.put('/reset-password', 'ResetPasswordController.update')
+
+Route.get('/files/:id', 'FileController.show')
+Route.post('/files', 'FileController.create')
